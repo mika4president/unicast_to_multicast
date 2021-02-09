57c09f84bb3e3dc85ef2d959518fc83807804436
Michel Kapelle <mawkapelle@gmail.com>
2021-02-09 12:17:13 +0100

    Update snapshotv2.sh

--- snapshotv2.sh
+++ snapshotv2.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 while true;do
-	ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1'  -timeout 1 -y -vframes 1 -q:v 2 /var/www/html/test.jpg >&2 ; echo 'RTSP ERROR, REBOOTING ' >> /home/pi/log.txt && sudo reboot now;
+	{ # try
+	    	ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1'  -timeout 1 -y -vframes 1 -q:v 2 /var/www/html/test.jpg
+	} || { # catch
+        	echo 'RTSP ERROR, REBOOTING ' >> /home/pi/log.txt && sudo reboot now;
+	}
 	sleep 60
 done
