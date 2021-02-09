e80a7748a800cb0fccf86cb108c7d1007a994cec
Michel Kapelle <mawkapelle@gmail.com>
2021-02-09 11:54:20 +0100

    Update snapshotv2.sh
    -y zodat file altijd overschreven wordt

--- snapshotv2.sh
+++ snapshotv2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true;do
-	ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1'  -timeout 1 -vframes 1 -q:v 2 /var/www/html/test.jpg >&2 ; echo 'RTSP ERROR, REBOOTING ' >> /home/pi/log.txt && sudo reboot now;
+	ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1'  -timeout 1 -y -vframes 1 -q:v 2 /var/www/html/test.jpg >&2 ; echo 'RTSP ERROR, REBOOTING ' >> /home/pi/log.txt && sudo reboot now;
 	sleep 60
 done
