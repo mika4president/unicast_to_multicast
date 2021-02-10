8fec4bb858b78a10ffb93d69e56f5a9728d07141
Michel Kapelle <mawkapelle@gmail.com>
2021-02-10 08:42:49 +0100

    Update snapshotv2.sh

--- snapshotv2.sh
+++ snapshotv2.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
-
+#!/bin/bash
 while true;do
-	{ # try
-	    	ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1'  -timeout 1 -y -vframes 1 -q:v 2 /var/www/html/test.jpg
-	} || { # catch
-        	echo 'RTSP ERROR, REBOOTING ' >> /home/pi/log.txt && sudo reboot now;
-	}
-	sleep 60
+ if  ffmpeg -ss 01:23:45 -i 'rtsp://admin:Camera123$@192.168.100.101:554/1/stream1/Profile1' -loglevel warning  -timeout 1 -y -vframes 1 -q:v 2 /var/www/html/test.jpg ; then # TRY
+    echo 'OK';
+ else # CATCH
+    echo 'RTSP ERROR!!!';
+    echo " `date -u` RTSP STREAM LOST " >> /var/log/log.txt
+    sleep 20;
+    sudo reboot now;
+ fi
+ sleep 60
 done
